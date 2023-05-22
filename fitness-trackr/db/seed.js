165e055d4b77f906a06bffb4ec5bc6f917d07e90
Brandon Taylor <bmt1994.tlh@gmail.com>
2023-05-22 11:28:36 -0400

    create tables

--- fitness-trackr/db/seed.js
+++ fitness-trackr/db/seed.js
@@ -1,34 +1,86 @@
 const client = require("./client");
 const {
-  users,
-  activities,
-  routines,
-  routine_activities,
+	users,
+	activities,
+	routines,
+	routine_activities,
 } = require("./seedData");
 
 async function dropTables() {
-  // Drop all tables in order
+	try {
+		console.log("Starting to drop tables...");
+
+		// have to make sure to drop in correct order
+		await client.query(`
+      DROP TABLE IF EXISTS post_tags;
+      DROP TABLE IF EXISTS tags;
+      DROP TABLE IF EXISTS posts;
+      DROP TABLE IF EXISTS users;
+    `);
+
+		console.log("Finished dropping tables!");
+	} catch (error) {
+		console.error("Error dropping tables!");
+		throw error;
+	}
 }
 
 async function createTables() {
-  // Define your tables and fields
+	try {
+		console.log("Starting to build tables...");
+
+		await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL
+      );
+
+      CREATE TABLE routines (
+        id SERIAL PRIMARY KEY,
+        creator_Id INTEGER REFERENCES users(id)
+        is_public BOOLEAN DEFAULT false,
+        name varchar(255) UNIQUE NOT NULL,
+        goal TEXT NOT NULL
+      );
+
+      CREATE TABLE activities (
+        id SERIAL PRIMARY KEY,
+        name varchar(255) UNIQUE NOT NULL,
+        description TEXT NOT NULL
+      );
+
+      CREATE TABLE routineActivities (
+        id SERIAL PRIMARY KEY,
+        routine_id INEGER REFERENCES routines(id)
+        activity_id INTEGER REGERENCES activities(id),
+        duration INTEGER,
+        count INTEGER
+      );
+    `);
+
+		console.log("Finished building tables!");
+	} catch (error) {
+		console.error("Error building tables!");
+		throw error;
+	}
 }
 
 async function populateTables() {
-  // Seed tables with dummy data from seedData.js
+	// Seed tables with dummy data from seedData.js
 }
 
 async function rebuildDb() {
-  client.connect();
-  try {
-    await dropTables();
-    await createTables();
-    await populateTables();
-  } catch (error) {
-    console.error(error);
-  } finally {
-    client.end();
-  }
+	client.connect();
+	try {
+		await dropTables();
+		await createTables();
+		await populateTables();
+	} catch (error) {
+		console.error(error);
+	} finally {
+		client.end();
+	}
 }
 
 rebuildDb();
