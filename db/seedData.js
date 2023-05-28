b5a03d7b57dad7167bf63ca509eecd8899b481c2
Brandon Taylor <bmt1994.tlh@gmail.com>
2023-05-28 12:06:23 -0400

    test

--- db/seedData.js
+++ db/seedData.js
@@ -6,6 +6,7 @@ const users = [
 	{ username: "Pawan", password: 1234567 },
 	{ username: "Chris", password: 12345678 },
 ];
+
 const activities = [
 	{ name: "bench press", description: "chest excercise" },
 	{ name: "squats", description: "leg day" },
@@ -13,28 +14,71 @@ const activities = [
 	{ name: "calf raises", description: "leg day" },
 	{ name: "planking", description: "abs" },
 ];
+
 const routines = [
 	{
 		creator_id: 1,
 		is_public: true,
-		name: "Exercise routine",
+		name: "Chest day",
 		goal: "get huge",
 	},
 	{
 		creator_id: 2,
 		is_public: false,
-		name: "Pilates class",
-		goal: "get in shape",
+		name: "Leg day",
+		goal: "get huge",
 	},
-];
-const routineActivities = [
+	{
+		creator_id: 1,
+		is_public: false,
+		name: "Biceps",
+		goal: "get huge",
+	},
 	{
 		creator_id: 2,
 		is_public: false,
-		name: "Pilates",
+		name: "Light leg day",
+		goal: "feel good",
+	},
+	{
+		creator_id: 1,
+		is_public: false,
+		name: "Abs",
 		goal: "get in shape",
 	},
-	{},
+];
+
+const routineActivities = [
+	{
+		routine_id: 1,
+		activity_id: 1,
+		duration: 1,
+		count: 10,
+	},
+	{
+		routine_id: 2,
+		activity_id: 2,
+		duration: 1,
+		count: 10,
+	},
+	{
+		routine_id: 3,
+		activity_id: 3,
+		duration: 1,
+		count: 10,
+	},
+	{
+		routine_id: 4,
+		activity_id: 4,
+		duration: 1,
+		count: 10,
+	},
+	{
+		routine_id: 5,
+		activity_id: 5,
+		duration: 1,
+		count: 10,
+	},
 ];
 
 module.exports = { users, activities, routines, routineActivities };
