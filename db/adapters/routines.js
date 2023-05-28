b3e6eec6197f545b78cc1f50c49b479e6d1841c8
Brandon Taylor <bmt1994.tlh@gmail.com>
2023-05-28 00:26:24 -0400

    added routines functions

--- db/adapters/routines.js
+++ db/adapters/routines.js
@@ -24,5 +24,134 @@ async function getAllRoutines() {
   `);
 	return rows;
 }
+async function getRoutineById(id) {
+	console.log("hello");
+	const { rows } = await client.query(
+		`
+    SELECT 
+      routines.id as id,
+      routines.name as name,
+      routines.goal as goal, 
+        CASE WHEN routine_activities.routine_id IS NULL THEN '[]'::json
+        ELSE 
+        JSON_AGG(
+          JSON_BUILD_OBJECT(
+          'id', activities.id,
+          'name', activities.name,
+          'description', activities.description,
+          'duration', routine_activities.duration,
+          'count', routine_activities.count
+          )
+        ) END AS activities
+        FROM routines
+        JOIN routine_activities 
+        ON routines.id = routine_activities.routine_id
+        JOIN activities 
+        ON routine_activities.activity_id = activities.id
+        WHERE routines.id = $1
+        GROUP BY routines.id, routine_activities.routine_id
+  `,
+		[id]
+	);
+	console.log({ rows });
+	return rows;
+}
+async function getRoutinesWithoutActivities() {
+	const {
+		rows: [routine],
+	} = await client.query(`SELECT * FROM routines`);
+}
+async function getAllRoutinesByUser(userId) {
+	try {
+		const {
+			rows: [routine],
+		} = await client.query(
+			`
+        SELECT *
+        FROM routines
+        WHERE creator_id = ${userId};
+        `,
+			[userId]
+		);
+	} catch (error) {
+		console.log(error);
+	}
+}
 
-module.exports = { createRoutine, getAllRoutines };
+async function getAllPublicRoutines() {
+	try {
+		const { rows } = await client.query(`
+    SELECT 
+      routines.id as id,
+      routines.name as name,
+      routines.goal as goal,
+      CASE WHEN routine_activities.routine_id IS NULL THEN '[]'::json
+      ELSE
+      JSON_AGG(
+        JSON_BUILD_OBJECT(
+          'id', activities.id,
+          'name', activities.name,
+          'description', activities.description,
+          'duration', routine_activities.duration,
+          'count', routine_activities.count
+          
+        )
+      ) END AS activities
+      FROM routines
+      FULL OUTER JOIN routine_activities 
+      ON routines.id = routine_activities.routine_id
+      FULL OUTER JOIN activities
+      ON activities.id = routine_activities.activity_id
+      WHERE routines.is_public = true
+      GROUP BY routines.id, routine_activities.routine_id
+    `);
+		return rows;
+	} catch (error) {
+		console.log(error);
+	}
+}
+async function getPublicRoutinesByUser(username) {
+	try {
+		const { rows } = client.query(
+			`
+      SELECT 
+        routines.id as id
+        routines.name as goal,
+        CASE WHEN routine_activities.routine_id IS NULL THEN '[]'::json
+        ELSE 
+        JSON_AGG(
+          JSon_BUILD_OBJECT(
+            'id', activities.id,
+            'name', activities.name,
+            'description', activities.description,
+            'duration', routine_activities.duration,
+            'count', routine_activities.count
+            )
+          ) END AS activities
+          FROM routines
+          JOIN users
+          ON routines.creator_id = users.id
+          FULL OUTER JOIN routines_activities
+          ON routines.id = routine_activities.routine_id
+          FULL OUTER JOIN activities 
+          ON activities.id = routine_activities.activity_id
+          WHERE users.username = $1 AND routines.is_public = true
+          GROUP BY routines.id, routines_activities.routine_id
+          `,
+			[username]
+		);
+		return rows;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+module.exports = {
+	createRoutine,
+	getAllRoutines,
+	getRoutineById,
+	getRoutinesWithoutActivities,
+	getAllPublicRoutines,
+	getPublicRoutinesByUser,
+	getAllRoutinesByUser,
+};
