9457aedcaab528153754a7c6d3071d127ae1bbd9
Brandon Taylor <bmt1994.tlh@gmail.com>
2023-06-01 10:48:37 -0400

    utils

--- routes/utils.js
+++ routes/utils.js
@@ -3,14 +3,16 @@ const jwt = require("jsonwebtoken");
 const authRequired = (req, res, next) => {
 	try {
 		const token = req.signedCookies.token;
-		console.log("Token: ", token);
-		jwt.verify(token, process.env.JWT_SECRET);
+		req.user = jwt.verify(token, process.env.JWT_SECRET);
+		console.log("Req.user: ", req.user);
 	} catch (error) {
 		res.status(401).send({
 			loggedIn: false,
-			message: "You are not authorized",
+			message: "You are not authorized!!!",
 		});
+		return;
 	}
 	next();
 };
+
 module.exports = { authRequired };
