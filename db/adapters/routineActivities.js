3cac90851e61398e6866745b0361a0c8b9000df3
Brandon Taylor <bmt1994.tlh@gmail.com>
2023-06-01 09:28:05 -0400

    added seed functions and routine activities

--- db/adapters/routineActivities.js
+++ db/adapters/routineActivities.js
@@ -1,25 +1,85 @@
 const client = require("../client");
 
 async function getRoutineActivityById(routineActivityId) {
-	const { rows } = await client.query(
-		`
+	try {
+		const {
+			rows: [routineActivity],
+		} = await client.query(
+			`
       SELECT * FROM routine_activities
-      WHERE id = ${routineActivityId};
-
+      WHERE id = $1
     `,
-		[routineActivityId]
-	);
-	return rows;
+			[routineActivityId]
+		);
+		return routineActivity;
+	} catch (error) {
+		throw error;
+	}
 }
 async function addActivityToRoutine(routine_id, activity_id, duration, count) {
-	const { rows } = await client.query(
-		`
+	try {
+		const { rows } = await client.query(
+			`
       INSERT INTO routine_activities(routine_id, activity_id, duration, count)
       VALUES ($1, $2, $3, $4)
-
+      RETURNING *;
     `,
-		[routine_id, activity_id, duration, count]
-	);
-	return rows;
+			[routine_id, activity_id, duration, count]
+		);
+		return rows[0];
+	} catch (error) {
+		throw error;
+	}
 }
-module.exports = { getRoutineActivityById, addActivityToRoutine };
+
+async function updateRoutineActivity(routineActivityId, count, duration) {
+	try {
+		const {
+			rows: [routineActivity],
+		} = await client.query(
+			` UPDATE routine_activities 
+            SET count = $2, duration = $3
+            WHERE id = $1
+            RETURNING *;
+        `,
+			[routineActivityId, count, duration]
+		);
+		return routineActivity;
+	} catch (error) {
+		throw error;
+	}
+}
+
+async function destroyRoutineActivity(routineActivityId) {
+	await client.query(
+		`DELETE from routine_activities
+        WHERE id = $1
+        `,
+		[routineActivityId]
+	);
+	return;
+}
+
+async function getRoutineActivityByRoutine(routineId) {
+	try {
+		const {
+			rows: [routine],
+		} = await client.query(
+			`
+        SELECT * FROM routine_activities
+        WHERE routine_id = $1
+      `,
+			[routineId]
+		);
+		return routine;
+	} catch (error) {
+		throw error;
+	}
+}
+module.exports = {
+	addActivityToRoutine,
+	getRoutineActivityById,
+	updateRoutineActivity,
+	destroyRoutineActivity,
+	getRoutineActivityByRoutine,
+};
