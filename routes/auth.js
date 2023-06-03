adeebf0d235ee6bf8ee892bef28a266c0f99a383
Brandon Taylor <bmt1994.tlh@gmail.com>
2023-06-02 22:18:41 -0400

    login function

--- routes/auth.js
+++ routes/auth.js
@@ -9,39 +9,56 @@ authRouter.post("/register", async (req, res, next) => {
 	try {
 		const { username, password } = req.body;
 
-		// Check if username exists already
 		const _user = await getUserByUsername(username);
 		if (_user) {
 			next({
 				message: "That user already exists!",
-				name: "Auth Error",
+				name: " Auth Error",
 			});
 			return;
 		}
-
 		const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+		console.log("hashed password:", hashedPassword);
 		const user = await createUser({ username, password: hashedPassword });
-		delete user.password;
-
+		console.log("JWT secret:", process.env.JWT_SECRET);
 		const token = jwt.sign(user, process.env.JWT_SECRET);
-
+		console.log("token:", token);
 		res.cookie("token", token, {
 			sameSite: "strict",
 			httpOnly: true,
 			signed: true,
 		});
-
-		res.send(user);
+		delete user.password;
+		res.send({ user, token });
 	} catch (error) {
 		next(error);
 	}
 });
+
 authRouter.post("/login", async (req, res, next) => {
 	try {
+		const { username, password } = req.body;
+
+		const user = await getUserByUsername(username);
+		const checkedpassword = await bcrypt.compare(password, user.password);
+		if (checkedpassword) {
+			delete user.password;
+			const token = jwt.sign(user, process.env.JWT_SECRET);
+			res.cookie("token", token, {
+				sameSite: "strict",
+				httpOnly: true,
+				signed: true,
+			});
+			res.send(user);
+		} else {
+			next({ message: "invalid login credentials" });
+			return;
+		}
 	} catch (error) {
 		next(error);
 	}
 });
+
 authRouter.get("/logout", async (req, res, next) => {
 	try {
 		res.clearCookie("token", {
