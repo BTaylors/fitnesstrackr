5aee15cda74effee3907ac3d886196404cf12c7b
Brandon Taylor <bmt1994.tlh@gmail.com>
2023-06-04 14:40:56 -0400

    activities router

--- routes/activities.js
+++ routes/activities.js
@@ -1,10 +1,25 @@
 const activitiesRouter = require("express").Router();
 const { getAllActivities } = require("../db/adapters/activities");
+const { getPublicRoutinesByActivity } = require("../db/adapters/routines");
 
 activitiesRouter.get("/", async (req, res, next) => {
 	try {
 		const activities = await getAllActivities();
 		res.send(activities);
+	} catch (error) {
+		next(error);
+	}
+});
+
+activitiesRouter.get("/:activityId/routines/", async (req, res, next) => {
+	try {
+		const { activityId } = req.params;
+		const publicRoutines = await getPublicRoutinesByActivity(activityId);
+		console.log("public routines:", publicRoutines);
+		res.send({
+			status_message: "getting public routines by activity",
+			data: publicRoutines,
+		});
 	} catch (error) {
 		next(error);
 	}
